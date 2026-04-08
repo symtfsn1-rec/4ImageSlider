47df345b11edd7684b69302ef583a10c73384d69
“snmrec” <symtfsn1@gmail.com>
2026-04-08 17:28:00 +0900

    first-commit

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
